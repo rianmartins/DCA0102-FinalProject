1df2ea348cf317b64be315d4a9e4b8cf88981730
rianmartins <rianlopes@hotmail.com>
2016-06-01 10:40:31 -0300

    Adicionada documentação necessária para o doxygen

--- mainwindow.h
+++ mainwindow.h
@@ -5,34 +5,60 @@
 #include <QMainWindow>
 #include <QDateTime>
 #include <QTimer>
+#include <QVector>
 
 namespace Ui {
 class MainWindow;
 }
-
-typedef struct{
-  QDateTime theTime; // hora da medida
-  float measurement; // medida
-} Entry;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+        * @brief MainWindow eh o construtor da classe principal
+        * @param parent eh um ponteiro para QWidget que indica
+            * onde a MainWindow sera desenhada
+    */
     explicit MainWindow(QWidget *parent = 0);
+    /**
+        *@brief ~MainWindow() eh o destrutor da classe
+    */
     ~MainWindow();
+    /**
+        *@brief getData() serve para pegar os dados gerados de determinado ip.
+        *@param ip é um parametro da classe Qstring para informar o ip desejado na consulta.
+    */
     void getData(QString ip);
+    /**
+        *@brief A função setConnections exibe os ips conectados com o servidor no listViewer para pode plotar o gráfico.
+    */
     void setConnections();
 
 public slots:
+    /**
+        *@brief TcpConnect, conecta com o servidor.
+    */
     void tcpConnect();
+    /**
+        *@brief Na função plotGraph, os dados que foram guardados a partir do produtor de dados para determinado ip é plotado em um gráfico com seus ultimos 10 valores.
+    */
     void plotGraph();
 
 private:
     Ui::MainWindow *ui;
+    /**
+        *@brief Socket de conexao com o servidor.
+    */
     QTcpSocket *socket;
+    /**
+        *@brief isConnected verifica se o cliente esta conectado com o servidor.
+    */
     bool isConnected;
+    /**
+        *@brief timer garante que a funcao plotGraph() continue verificando se há novos valores a serem plotados.
+    */
     QTimer *timer;
 };
 
