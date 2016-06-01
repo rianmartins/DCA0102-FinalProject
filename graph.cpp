d71d85ba76bc8130fa7b6ff4caa03cbf4b46ba70
rianmartins <rianlopes@hotmail.com>
2016-06-01 10:04:08 -0300

    Update graph.cpp
    Atualização que corrige erro no plot inicial dos primeiros valores no gráfico

--- graph.cpp
+++ graph.cpp
@@ -7,7 +7,6 @@
 
 Graph::Graph(QWidget *parent) : QWidget(parent)
 {
-    startTimer(20);
     max = -20;
 }
 
@@ -20,6 +19,7 @@ void Graph::setValues(QDateTime t1, float v1)
     uint t = t1.toTime_t() - date.at(0).toTime_t();
     float timeGraph = (float) t;
     time.push_back(timeGraph);
+    repaint();
 }
 
 void Graph::clean()
@@ -42,19 +42,70 @@ void Graph::paintEvent(QPaintEvent *e)
     painter.setBrush(brush);
     painter.drawRect(0,0,width()-1,height()-1);
 
-    pen.setColor("blue");
+    pen.setColor("red");
     painter.setPen(pen);
 
-    for(int i = 0; i < value.size(); i++){
-        float v = height() - (value.at(i)/max)*height();
-        float t = time.at(i)/time.at(time.length()-1) * width();
-        painter.drawPoint(t,v);
-        if(i>0)
-            painter.drawLine(time.at(i-1)/time.at(time.length()-1) * width(), height() - (value.at(i-1)/max)*height(),t,v);
+    if(value.size()>33){
+        float nMax = -20;
+
+        time1.clear();
+        value1.clear();
+
+        for(uint i = time.size()-10; i < time.size(); i++){
+            time1.push_back(time[i]);
+            value1.push_back(value[i]);
+        }
+
+        for(int i = 9; i >= 0; i--){
+            if(value1.at(i) > nMax)
+                nMax = value1.at(i);
+            time1[i] = time1.at(i)-time1.at(0);
+        }
+        max = nMax;
+
+        for(uint i = 0; i < value1.size(); i++){
+            float v = height() - (value1.at(i)/max)*height();
+            float t = time1.at(i)/time1.at(time1.size()-1) * width();
+            painter.drawPoint(t,v);
+            if(i>0)
+                painter.drawLine(time1.at(i-1)/time1.at(time1.size()-1) * width(), height() - (value1.at(i-1)/max)*height(),t,v);
+        }
+    }
+    else{
+        float t0, v0, t, v;
+        if(time.size()>0)
+        {
+            t0 = 0;
+            v0 = height() - (value.at(0)/max)*height();
+
+            for(uint i = 1; i < value.size(); i++){
+                t = time.at(i)/30 * width();
+                v = height() - (value.at(i)/max)*height();
+
+                if(t>0)
+                    painter.drawLine(QPointF(t0,v0),QPointF(t,v));
+
+                t0 = t;
+                v0 = v;
+            }
+        }
     }
 }
 
-void Graph::timerEvent(QTimerEvent *e)
+float Graph::getMaxValue()
 {
-    repaint();
+    if(value.size()<10)
+    {
+        return max;
+    }
+    else
+    {
+        float nmax = 0;
+        for(uint i = 0; i < value1.size(); i++){
+            if(value1.at(i) > nmax)
+                nmax = value1.at(i);
+        }
+        return nmax;
+    }
+
 }
